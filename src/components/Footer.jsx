69d4c5bdacffd9c3abae3a008adcff4666ab0cb7
bjarne <bjarnewilhelmhaugen@hotmail.com>
2025-05-19 10:56:20 +0200

    fixed stuff in footer and trying to get to post to github pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import "../css/Footer.css";
 
 const Footer = () => (
   <footer>
+    <div className="footerBarCut"></div>
     <div className="footerBar">
-      <h3 className="whiteText">UX by</h3>
-      <h3 className="yellowText">Eirik Granmorken</h3>
-      <img className="linkedin" src="" alt="" />
-      <img className="github" src="" alt="" />
-      <h3 className="whiteText">+ 47 93 80 15 64</h3>
-      <h3 className="whiteText"> bjarnewilhelmhaugen@hotmail.com</h3>
+      <div className="innerFooterBar">
+        <div className="eirikLink">
+          <h3 className="whiteText">UX by</h3>
+          <h3 className="yellowText">Eirik Granmorken</h3>
+        </div>
+        <img className="icon" src="/linkedin.svg" alt="linkedin" />
+        <img className="icon" src="/github.svg" alt="github" />
+
+        <h3 className="whiteText">+ 47 93 80 15 64 </h3>
+
+        <h3 className="whiteText">bjarnewilhelmhaugen@hotmail.com</h3>
+      </div>
     </div>
-  </footer>
+  </footer >
 );
 
 export default Footer;
