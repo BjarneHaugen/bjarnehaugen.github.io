c6597fbfe748a9c005cfab97e410c2b1176ebb7b
bjarne <bjarnewilhelmhaugen@hotmail.com>
2025-05-27 14:46:19 +0200

    added missing tag in landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -20,13 +20,13 @@ const LandingPage: React.FC = () => {
                 <div className="aboutMeRow">
                     <div className="aboutMeText">
                         <h2 className='whiteText fontRajdhani fontStyleSemiBold fontSizeMedium'>Hei jeg heter</h2>
-                        <h1><span className="whiteText fontBebasNeue fontStyleNormal fontSizeXLarge">BJARNE</span> <span className="yellowText fontBebasNeue fontSizeXLarge">HAUGEN</span></h1>
+                        <h1><span className="whiteText fontBebasNeue fontStyleNormal fontSizeXLarge">BJARNE</span> <span className="yellowText fontBebasNeue fontStyleNormal fontSizeXLarge">HAUGEN</span></h1>
                         <p id='aboutMeSummary' className='whiteText fontRajdhani fontStyleNormal fontSizeMedium'> her kommer det tekst </p>
                         <button
                             id="aboutMeButton"
                             className='fontBebasNeue fontStyleNormal fontSizeMedium'
                             onClick={() => window.location.hash = '#/aboutMe'}
-                        > 
+                        >
                             Hvem er jeg?
                         </button>
                     </div>
