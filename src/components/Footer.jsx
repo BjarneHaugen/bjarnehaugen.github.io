cc0d640b5a8a5ec79ffe76a14dc0d4874718d852
bjarne <bjarnewilhelmhaugen@hotmail.com>
2025-05-27 14:01:47 +0200

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,16 +7,8 @@ const Footer = () => (
     <div className="footerBar">
       <div className="innerFooterBar">
         <div className="eirikLink">
-          <h3 className="whiteText fontInter">UX by</h3>
-          <h3 className="yellowText fontInter">Eirik Granmorken</h3>
+          <h4 className="whiteText fontInter">UX by <span className="yellowText fontInter">Eirik Granmorken</span> </h4>
         </div>
-        <img className="icon" src="/linkedin.svg" alt="linkedin" onClick={() => window.open('https://www.linkedin.com/in/bjarne-haugen/', '_blank')} style={{ cursor: 'pointer' }} />
-        <img id="github" className="icon" src="/github.svg" alt="github" onClick={() => window.open('https://github.com/bjarnehaugen', '_blank')} style={{ cursor: 'pointer' }} />
-        <img className="icon" src="/phone.svg" alt="phone" />
-        <h3 className="whiteText fontInter">+ 47 93 80 15 64 </h3>
-
-        <img className="icon" src="/email.svg" alt="email" />
-        <h3 className="whiteText fontInter">bjarnewilhelmhaugen@hotmail.com</h3>
       </div>
     </div>
   </footer >
